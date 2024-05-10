051e1f7d59901e226ea410af026f2ea9f75c9f27
Ulises-Islas <emyises18012@gmail.com>
2024-05-09 18:13:15 -0600

    first commit

---
+++ src/main/java/me/ulises/PokeApiTechnicalTest/PokeApiTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package me.ulises.PokeApiTechnicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiTechnicalTestApplication.class, args);
+	}
+
+}
